dc7b9ec032b867d89b65f9619f7053d495d15cf5
Oscar00v <Oscar00v@gmail.com>
2024-07-08 21:34:52 -0600

    For loops

---
+++ main.js
@@ -0,0 +1,146 @@
+
+
+
+for (let indice = 1; indice <= 50; indice++){
+  
+    if (indice % 2 !== 0){ // 
+  
+      console.log(indice)
+  
+    }
+  
+  }
+  
+  
+  let arreglo = [3, "Hola", true, 5, 19, "Vanessa", false]
+  
+  console.log("Elementos numericos de mi arreglo:")
+  
+  for (let i = 0; i < arreglo.length; i++){
+  
+    if(typeof arreglo[i] === "number"){
+  
+      console.log(arreglo[i]);
+  
+    }
+  
+  }
+
+
+
+let pokemons = [ 'bulbasaur',
+  'ivysaur',
+  'venusaur',
+  'charmander',
+  'charmeleon',
+  'charizard',
+  'squirtle',
+  'wartortle',
+  'blastoise',
+  'caterpie',
+  'metapod',
+  'butterfree',
+  'weedle',
+  'kakuna',
+  'beedrill',
+  'pidgey',
+  'pidgeotto',
+  'pidgeot',
+  'rattata',
+  'raticate',
+  'spearow',
+  'fearow',
+  'ekans',
+  'arbok',
+  'pikachu',
+  'raichu',
+  'sandshrew',
+  'sandslash',
+  'nidoran-f',
+  'nidorina',
+  'nidoqueen',
+  'nidoran-m',
+  'nidorino',
+  'nidoking',
+  'clefairy',
+  'clefable',
+  'vulpix',
+  'ninetales',
+  'jigglypuff',
+  'wigglytuff',
+  'zubat',
+  'golbat',
+  'oddish',
+  'gloom',
+  'vileplume',
+  'paras',
+  'parasect',
+  'venonat',
+  'venomoth',
+  'diglett',
+  'dugtrio',
+  'meowth',
+  'persian',
+  'psyduck',
+  'golduck',
+  'mankey',
+  'primeape',
+  'growlithe',
+  'arcanine',
+  'poliwag',
+  'poliwhirl',
+  'poliwrath',
+  'abra',
+  'kadabra',
+  'alakazam',
+  'machop',
+  'machoke',
+  'machamp',
+  'bellsprout',
+  'weepinbell',
+  'victreebel',
+  'tentacool',
+  'tentacruel',
+  'geodude',
+  'graveler',
+  'golem',
+  'ponyta',
+  'rapidash',
+  'slowpoke',
+  'slowbro',
+  'magnemite',
+  'magneton',
+  'farfetchd',
+  'doduo',
+  'dodrio',
+  'seel',
+  'dewgong',
+  'grimer',
+  'muk',
+  'shellder',
+  'cloyster',
+  'gastly',
+  'haunter',
+  'gengar',
+  'onix',
+  'drowzee',
+  'hypno',
+  'krabby',
+  'kingler',
+  'voltorb' ]
+
+
+
+
+
+  let num = parseInt( prompt("dame un numero:"));
+
+  for (let indice = 1; indice <= num; indice++){
+  
+    if (indice % 5 == 0){ // 
+  
+        console.log(pokemons[indice]);
+  
+    }
+  
+  }
